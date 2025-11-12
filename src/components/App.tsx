0e5a02c7a1cf800f328020fd74ec89e0f45bb10c
FigueroaMartin <95000222+FigueroaMartin@users.noreply.github.com>
2025-11-11 21:14:11 -0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -96,6 +96,7 @@ export default function App() {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   />
                 </svg>
+                <img src="https://res.cloudinary.com/diytuytti/image/upload/v1760414538/1cdf732a-a38b-4552-9baa-8f0b0018dfd1.png"></img>
                 <p className="text-xl">Selecciona un canal para comenzar</p>
                 <p className="text-sm mt-2">O crea uno nuevo desde el panel izquierdo</p>
               </div>
